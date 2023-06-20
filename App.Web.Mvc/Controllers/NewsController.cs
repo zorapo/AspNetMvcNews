8d3153cc45d81083663f06b05c33b61c43396e6a
aysemineokyay <User@DESKTOP-NNOFQ3O>
2023-06-20 22:50:36 +0300

    Form gönderme alert düzenlendi

--- App.Web.Mvc/Controllers/NewsController.cs
+++ App.Web.Mvc/Controllers/NewsController.cs
@@ -1,21 +1,20 @@
-﻿using App.Entities.Dtos.NewsCommentDtos;
+﻿using App.Entities.Concrete;
 using App.Service.Abstract;
 using App.Shared.Utilities.Results.ComplexTypes;
 using App.Web.Mvc.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using System.Text.Json.Serialization;
-using System.Text.Json;
-using App.Entities.Concrete;
+using System.Reflection.Metadata;
+
 
 namespace App.Web.Mvc.Controllers
 {
-    public class NewsController : Controller
-    {
-        private readonly INewsService _newsService;
-        private readonly ICategoryService _categoryService;
-        private readonly INewsCommentService _newsCommentService;
-        private readonly UserManager<User> _userManager;
+	public class NewsController : Controller
+	{
+		private readonly INewsService _newsService;
+		private readonly ICategoryService _categoryService;
+		private readonly INewsCommentService _newsCommentService;
+		private readonly UserManager<User> _userManager;
 
 		public NewsController(INewsService newsService, ICategoryService categoryService, INewsCommentService newsCommentService, UserManager<User> userManager)
 		{
@@ -26,25 +25,25 @@ namespace App.Web.Mvc.Controllers
 		}
 
 		public async Task<IActionResult> Search(string keyword, int? pageNo)
-        {
-            int pageSize = 6;
-            int pageIndex = 1;
+		{
+			int pageSize = 6;
+			int pageIndex = 1;
 
-            pageIndex = pageNo.HasValue ? Convert.ToInt32(pageNo) : 1;
-            var search = await _newsService.SearchAsync(keyword, pageIndex, pageSize, false);
-            if (search != null)
-            {
-                return View(new SearchViewModel
-                {
-                    NewsList = search,
-                    Keyword = keyword,
-                });
-            }
-            return NotFound();
-        }
-        //[Route("{title}/{id}")]
-        public async Task<IActionResult> Detail(int id)
-        {
+			pageIndex = pageNo.HasValue ? Convert.ToInt32(pageNo) : 1;
+			var search = await _newsService.SearchAsync(keyword, pageIndex, pageSize, false);
+			if (search != null)
+			{
+				return View(new SearchViewModel
+				{
+					NewsList = search,
+					Keyword = keyword,
+				});
+			}
+			return NotFound();
+		}
+		//[Route("{title}/{id}")]
+		public async Task<IActionResult> Detail(int id)
+		{
 			var news = await _newsService.GetAsync(id);
 			var comments = await _newsCommentService.GetNewsCommentsAsync(id);
 
@@ -56,19 +55,29 @@ namespace App.Web.Mvc.Controllers
 
 		}
 		[HttpPost]
-		public async Task<IActionResult> Detail(NewsAndCommentsViewModel model,int id)
+		public async Task<IActionResult> Detail(NewsAndCommentsViewModel model, int id)
 		{
-			var user = await _userManager.FindByEmailAsync(model.NewsComments.Email);
-			model.NewsComments.UserId = user.Id;
-			model.NewsComments.NewsId= id;	
-			var result = await _newsCommentService.AddAsync(model.NewsComments);
-			if (result.ResultStatus == ResultStatus.Success)
+			if (model.NewsComments.Email == null)
 			{
-				TempData["Message"] = "Başarılı işlem";
-				return RedirectToAction("Detail",id);
+				TempData["MessageError"] = "Unexpected error ! Please try again.";
+				return RedirectToAction("Detail", id); ;
 			}
-			ModelState.AddModelError("", result.Message);
-			TempData["Message"] = "Hatalı işlem";
+			var user = await _userManager.FindByEmailAsync(model.NewsComments.Email);
+			if (user != null)
+			{
+
+				model.NewsComments.UserId = user.Id;
+				model.NewsComments.NewsId = id;
+				var result = await _newsCommentService.AddAsync(model.NewsComments);
+				if (result.ResultStatus == ResultStatus.Success)
+				{
+					TempData["MessageSuccess"] = "Your comment send successfully. Please wait for confirm your comment.";
+					return RedirectToAction("Detail", id);
+				}
+				ModelState.AddModelError("", result.Message);
+				TempData["MessageError"] = "Unexpected error ! Please try again.";
+			}		
+			TempData["MessageError"] = "Unexpected error ! Please try again.";
 			return RedirectToAction("Detail", id); ;
 
 		}
